b7beb5a6a68bdf488dfa947b1563658a6d21816a
IriTok <199002139+IriTok@users.noreply.github.com>
2026-05-10 11:08:44 +0500

    Create project: practical 1

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Admin\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'admin', 'namespace' =>'Admin'], function() {
+    Route::get('/', [MainController::class, 'index'])->name('admin.index');
 });
