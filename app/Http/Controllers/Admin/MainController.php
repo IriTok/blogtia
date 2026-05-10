e6a87d89f7d0783ad40f9a6d5921edf46b983640
IriTok <199002139+IriTok@users.noreply.github.com>
2026-05-10 15:25:08 +0500

    Add relationships: practical 4

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 
 class MainController extends Controller
 {
     public function index() 
     {
+        $tag = new Tag();
+        $tag->title = 'гав';
+        $tag->save();
         return view ('admin.index');
     }
 }
